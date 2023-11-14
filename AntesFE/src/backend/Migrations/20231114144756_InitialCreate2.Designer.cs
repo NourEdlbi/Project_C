225ce8db8e4083eca2332fa9981b6e49473395d9
AngelovdBroek <1055539@hr.nl>
2023-11-14 15:49:46 +0100

    Password added to account
    Forgot to add password in account table, this has now been added

--- AntesFE/src/backend/Migrations/20231114144756_InitialCreate2.Designer.cs
+++ AntesFE/src/backend/Migrations/20231114144756_InitialCreate2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using YourNamespace;
@@ -11,9 +12,11 @@ using YourNamespace;
 namespace backend.Migrations
 {
     [DbContext(typeof(ForumContext))]
-    partial class ForumContextModelSnapshot : ModelSnapshot
+    [Migration("20231114144756_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AntesFE/src/backend/Model.cs
+++ AntesFE/src/backend/Model.cs
@@ -104,6 +104,7 @@ namespace YourNamespace
         [Key]
         public int ID { get; set; }
         public string Name { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public bool Admin { get; set; }
 
