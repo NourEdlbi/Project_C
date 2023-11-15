8160776afe27ea4d35f49872e9bd32ee672de46c
NourEdlbi <93190628+NourEdlbi@users.noreply.github.com>
2023-11-15 11:54:48 +0100

    removed backend adden AntesBE

--- AntesBE/obj/Debug/net7.0/AntesBE.AssemblyInfo.cs
+++ AntesBE/obj/Debug/net7.0/AntesBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AntesBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5229783b9b35afb3bbe95b91983d916b207d13a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AntesBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AntesBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
