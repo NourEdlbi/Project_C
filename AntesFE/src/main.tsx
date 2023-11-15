cacf5280f1a9f06e32b651b38709c01eea8e7c1c
wik018 <0992258@hr.nl>
2023-11-15 15:41:34 +0100

    Update main.tsx

--- AntesFE/src/main.tsx
+++ AntesFE/src/main.tsx
@@ -1,35 +1,28 @@
-<<<<<<< HEAD
 /*
  *   Copyright (c) 2023 
  *   All rights reserved.
  */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
-import './index.css'
-=======
 
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
->>>>>>> main
 import ErrorPage from "./error-page";
 import Login from './routes/Loginpage.tsx';
 import Info from './routes/Info.tsx';
-import Inst from './routes/Instellingen.tsx';
+import Inst from './routes/instellingen.tsx';
 import PasswordReset from './routes/PasswordReset.tsx';
 import Profiel from './routes/Profile.tsx';
 
 import ASidebar from './routes/Admin/adminSidebar.tsx';
 import Users from './routes/Admin/AdminUsers.tsx';
-import AForum from './routes/Admin/AdminForum.tsx';
+import AForum from './routes/Admin/adminForum.tsx';
 import Aquiz from './routes/Admin/AdminQuiz.tsx';
 import AdminAgenda from './routes/Admin/AdminAgenda.tsx';
 
 import USidebar from './routes/User/UserSidebar.tsx';
-import Uagenda from './routes/User/UserAgenda.tsx';
-import Uforum from './routes/User/UserForum.tsx';
+import Uagenda from './routes/User/userAgenda.tsx';
+import Uforum from './routes/User/userForum.tsx';
 import Uhome from './routes/User/UserHomepage.tsx';
 import Uquiz from './routes/User/UserQuiz.tsx';
 
@@ -52,70 +45,6 @@ function Main() {
         }, 100); // Check every 100ms
 
 
-<<<<<<< HEAD
-const router = createBrowserRouter([
-    {
-        path: "/",
-        //element: <Sidebar />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <Login/>,
-            },
-            {
-                path: "adminSidebar", // The path for adminSidebar
-                element: <ASidebar />, // Render the ASidebar component directly
-                children: [
-                    {
-                        path: "users",
-                        element: <Users/>,
-                    },
-                    {
-                        path: "adminForum",
-                        element: <AForum/>,
-                    },
-                    
-                ],
-                
-            },
-            {
-                path: "userSidebar", // The path for userSidebar
-                element: <USidebar />, // Render the USidebar component directly
-                children: [
-                    {
-                        path: "Profile",
-                        element: <Profiel/>,
-                    },
-                    {
-                        path: "agenda",
-                        element: <Uagenda/>,
-                    },
-                    {
-                        path: "userForum",
-                        element: <Uforum/>,
-                    },
-                    
-                ],
-            },
-            {
-                path: "agenda",
-                element: <Agenda />,
-            },
-            {
-                path: "Login",
-                element: <Login />
-            },
-            {
-                path: "Informatie",
-                element: <Info />
-            },
-            {
-                path: "inst",
-                element: <Inst />
-            },
-        ],
-=======
         return () => clearInterval(interval);
     }, [isNightMode]);
     
@@ -199,7 +128,6 @@ const router = createBrowserRouter([
             ],
         },
     ]);
->>>>>>> main
 
 
     return (
