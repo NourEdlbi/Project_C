5ff31a4db7de017512b76eeeeed00afd1205219d
NourEdlbi <93190628+NourEdlbi@users.noreply.github.com>
2023-11-20 13:38:54 +0100

    Update UserQuiz.tsx

--- AntesFE/src/routes/User/UserQuiz.tsx
+++ AntesFE/src/routes/User/UserQuiz.tsx
@@ -1,47 +1,50 @@
 import "./UserQuiz.css";
-
-const getquizzes = [
-    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
-    { id: 2, title: 'Installation', content: 'You can install React from npm.' }
-];
-
-const quizzes = getquizzes.map((quiz) =>
-    <div key={quiz.id}>
-        <button onClick={() => openQuiz(quiz.id)} className="quizbanner"> {quiz.title} </button>
-        <div id={quiz.id.toString() } className="quizdescription">
-            <p>{quiz.content}</p>
-
-            <button className="Gotoquizbutton">
-                Make Quiz
-            </button>
-        </div>
-        
-    </div>
-);
-function openQuiz(id) {
-    const x = document.getElementById(id) as HTMLElement;
-    if (x.style.display == "block") {
-        x.style.display = "none";
-    }
-    else {
-        x.style.display = "block";
-    }
-
-}
+import { useNavigate } from 'react-router-dom';
 
 export default function Uquiz() {
+    const navigate = useNavigate();
+    const getquizzes = [
+        { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+        { id: 2, title: 'Installation', content: 'You can install React from npm.' }
+    ];
+
+    const quizzes = getquizzes.map((quiz) =>
+        <div key={quiz.id}>
+            <button onClick={() => openQuiz(quiz.id)} className="quizbanner"> {quiz.title} </button>
+            <div id={quiz.id.toString()} className="quizdescription">
+                <p>{quiz.content}</p>
+
+                <button onClick={() => navigateToQuiz(quiz.id)} className="Gotoquizbutton">
+                    Make Quiz
+                </button>
+            </div>
+        </div>
+    );
+
+    function openQuiz(id) {
+        const x = document.getElementById(id) as HTMLElement;
+        if (x.style.display == "block") {
+            x.style.display = "none";
+        }
+        else {
+            x.style.display = "block";
+        }
+
+    }
+
+    function navigateToQuiz(id) {
+        const route = "/userSidebar/Quizzes/" + { id };
+        navigate(route);
+    }
+
     return (
         <div>
             <div className="titel">
                 <h1> Quiz</h1>
             </div>
             <div className="quizzes">
-
-                { quizzes}
-                
+                {quizzes}
             </div>
-            
         </div>
-
     )
 }
