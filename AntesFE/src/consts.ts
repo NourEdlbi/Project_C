07b46087dee22119be8d73779a5d0679d59bb130
NourEdlbi <93190628+NourEdlbi@users.noreply.github.com>
2023-12-01 10:02:20 +0100

    ignore base url file

--- AntesFE/src/consts.ts
+++ AntesFE/src/consts.ts
@@ -2,5 +2,5 @@
  *   Copyright (c) 2023 
  *   All rights reserved.
  */
-export const BASE_URL = "http://localhost:5263";
-//export const BASE_URL = "https://localhost:7109";
+//export const BASE_URL = "http://localhost:5263";
+export const BASE_URL = "https://localhost:7109";
