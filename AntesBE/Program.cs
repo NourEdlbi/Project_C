39eeec4b2e75e07eb0e6d6f172fde3fc49b73dc9
NourEdlbi <93190628+NourEdlbi@users.noreply.github.com>
2023-12-05 15:33:35 +0100

    added origin

--- AntesBE/Program.cs
+++ AntesBE/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
+            policy.WithOrigins("http://localhost:5173", "https://localhost:5173", "http://127.0.0.1:5173")
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
