ce8e1fa127af9f72a5be01e1aaa4ae4912e8065c
mr_lxix <abdelrahiemm@gmail.com>
2023-12-09 11:37:25 +0100

    show password toegevoegd
    gebruiker kan kiezen om zijn ingevoerde wachtwoord te zien heb grappige emojis toegevoegd. in de comment staat hoe je een ander emoji kan gebruiken.

--- AntesFE/src/routes/Loginpage.tsx
+++ AntesFE/src/routes/Loginpage.tsx
@@ -5,104 +5,113 @@ import menuImage from '../assets/Anteslogo.png';
 import { BASE_URL } from '../consts.ts';
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate();
-    const [userInfos, setUserInfos] = useState<userinfoInterface | null>(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+  const [userInfos, setUserInfos] = useState<userinfoInterface | null>(null);
 
-    interface userinfoInterface {
-        id: number;
-        name: string;
-        email: string;
-        admin: boolean;
-    }
+  interface userinfoInterface {
+    id: number;
+    name: string;
+    email: string;
+    admin: boolean;
+  }
 
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
-    const handleResetPassword = () => {
-        navigate('/password-reset');
-    };
+  const handleResetPassword = () => {
+    navigate('/password-reset');
+  };
 
-    const handleLogin = async () => {
-        try {
-            const response = await fetch(`${BASE_URL}/Login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email,
-                    wachtwoord: password,
-                }),
-            });
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/Login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          wachtwoord: password,
+        }),
+      });
 
-            const data = await response.json();
-            localStorage.setItem('Userinfo', JSON.stringify(data));
-            setUserInfos(data);
+      const data = await response.json();
+      localStorage.setItem('Userinfo', JSON.stringify(data));
+      setUserInfos(data);
 
-            // Use the callback form of setState to ensure you get the latest state
-            setUserInfos((prevState) => {
-                localStorage.setItem('Userinfo', JSON.stringify(data));
+      setUserInfos((prevState) => {
+        localStorage.setItem('Userinfo', JSON.stringify(data));
 
-                if (data.admin) {
-                    navigate('/adminSidebar');
-                } else {
-                    navigate('/userSidebar/welcome');
-                }
-
-                return data;
-            });
-        } catch (error) {
-            console.error(error);
-            setErrorMessage('Incorrect email or password');
+        if (data.admin) {
+          navigate('/adminSidebar');
+        } else {
+          navigate('/userSidebar/welcome');
         }
-    };
 
-    return (
-        <div className="login">
-            <img
-                src={menuImage}
-                alt="Logo"
-                style={{ width: '300px', height: 'auto', position: 'relative', bottom: '5rem' }}
-            />
-            <h1>Login</h1>
-            <form>
-                <div className="form-group">
-                    <label>Email: </label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                        placeholder="Email"
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Wachtwoord: </label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                        placeholder="Wachtwoord"
-                    />
-                </div>
-                {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
-                <button type="button" style={{ marginRight: '15px' }} onClick={handleLogin}>
-                    Inloggen
-                </button>
-                <button type="button" onClick={handleResetPassword}>
-                    Wachtwoord Vergeten
-                </button>
-                <br />
-                <br />
-                {/* Add additional buttons or UI elements as needed */}
-            </form>
+        return data;
+      });
+    } catch (error) {
+      console.error(error);
+      setErrorMessage('Incorrect email or password');
+    }
+  };
+
+  return (
+    <div className="login">
+      <img
+        src={menuImage}
+        alt="Logo"
+        style={{ width: '300px', height: 'auto', position: 'relative', bottom: '5rem' }}
+      />
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label>Email: </label>
+          <input type="email" value={email} onChange={handleEmailChange} placeholder="Email" />
         </div>
-    );
+        <div className="form-group">
+          <label>Wachtwoord: </label>
+          <div style={{ position: 'relative' }}>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Wachtwoord"
+            />
+            <span
+              style={{
+                position: 'absolute',
+                right: '10px',
+                top: '35%',
+                transform: 'translateY(-50%)',
+                cursor: 'pointer',
+              }}
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {/* Emoji kan je veranderen door een andere te plakken op de gewenste plek. */}
+              {showPassword ? '🙉' : '🙈'}
+            </span>
+          </div>
+        </div>
+        {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+        <button type="button" style={{ marginRight: '15px' }} onClick={handleLogin}>
+          Inloggen
+        </button>
+        <button type="button" onClick={handleResetPassword}>
+          Wachtwoord Vergeten
+        </button>
+        <br />
+        <br />
+      </form>
+    </div>
+  );
 }
