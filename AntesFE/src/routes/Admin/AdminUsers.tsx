9a0b669a803b34eca66e4101c6330aa89cdd5b96
mr_lxix <abdelrahiemm@gmail.com>
2023-12-18 13:34:23 +0100

    bugs bij uitnodiging mail weg gehaald.
    Als je de naam intikte kreeg je nooit de naam te zien wat er in de mail staat mu wel.

--- AntesFE/src/routes/Admin/AdminUsers.tsx
+++ AntesFE/src/routes/Admin/AdminUsers.tsx
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
 export default function AddUser() {
-  const [users, setUsers] = useState([]);
-  const [emailSent, setEmailSent] = useState(false); // State to track email sending status
-  const [emailSendingError, setEmailSendingError] = useState(""); // State to track email sending errors
+  const [emailSent, setEmailSent] = useState(false);
+  const [emailSendingError, setEmailSendingError] = useState("");
 
-  // Function to handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    setEmailSent(false); // Reset the email sent status on new submission
-    setEmailSendingError(""); // Reset the email sending error on new submission
+    setEmailSent(false);
+    setEmailSendingError("");
 
     const newUser = {
       name: event.target.elements.name.value,
@@ -18,32 +16,31 @@ export default function AddUser() {
       email: event.target.elements.email.value
     };
 
-    // Add the user to the state
-    setUsers(prevUsers => [...prevUsers, newUser]);
-
-    // Prepare the parameters to send in the email
     const templateParams = {
-      from_name: newUser.name + ' ' + newUser.surname,
-      to_email: newUser.email, // Email address from the form
+      from_name: 'Team Antes',
+      to_email: newUser.email,
       reply_to: 'your_email@example.com',
+      // Placeholders must be the same like in emailjs.com!!
+      name: newUser.name,
+      surname: newUser.surname,
+      email: newUser.email
     };
 
-    // Send the email
     emailjs.send(
-      'service_hto0evy', // Service ID from EmailJS
-      'template_l05r34q', // Template ID from EmailJS
+      'service_hto0evy',
+      'template_l05r34q',
       templateParams,
-      'fY4BkSACibEGz5Y0e' // User ID from EmailJS
+      'fY4BkSACibEGz5Y0e'
     )
     .then(response => {
       console.log('SUCCESS!', response.status, response.text);
-      setEmailSent(true); // Set email sent status to true on success
-      event.target.reset(); // Clear the form fields
+      setEmailSent(true);
+      event.target.reset();
     })
     .catch(err => {
       console.error('FAILED...', err);
-      setEmailSent(false); // Keep email sent status as false on failure
-      setEmailSendingError("Failed to send email. Please try again later."); // Set the error message
+      setEmailSent(false);
+      setEmailSendingError("Email versturen niet gelukt. Probeer het later opnieuw.");
     });
   };
 
@@ -69,8 +66,11 @@ export default function AddUser() {
           <button type="submit">Send Email</button>
         </div>
       </form>
-      {emailSent && <p>Mail is verzonden!</p>}
+      {emailSent && <p>Email verstuurd!!</p>}
       {emailSendingError && <p className="error-message">{emailSendingError}</p>}
     </div>
   );
 }
+
+
+
